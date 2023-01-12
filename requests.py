1673d9cdcaacd6bff543758620612ae0de82c64a
jimsyoung <122297150+jimsyoung@users.noreply.github.com>
2023-01-12 17:01:44 -0500

    Update requests.py

--- requests.py
+++ requests.py
@@ -4,4 +4,4 @@ streamlit.header('Fruityvice Fruit Advice!')
 #New Section to display fruityvice api response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
